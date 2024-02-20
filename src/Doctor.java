d71db2ad2c00ab8173225340a2e6d795b713110a
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2024-02-20 17:13:13 -0400

    FIRST COMMIT

---
+++ src/Doctor.java
@@ -0,0 +1,20 @@
+public class Doctor {
+    static int id = 0; // Autoincrement
+    String name;
+    String speciality;
+    Doctor() {
+        System.out.println("Construyendo el Obejeto Doctor");
+        id++;
+    }
+    Doctor(String name) {
+        System.out.println("El nombre del doctor a cargo es " + name);
+    }
+
+    // Comportamientos
+    public void showName() {
+        System.out.println(name);
+    }
+    public void showId() {
+        System.out.println("ID doctor: " + id);
+    }
+}
