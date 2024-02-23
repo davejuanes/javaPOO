2c666d0868557e06f162de27b7d51d0186b1ea1e
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2024-02-23 19:13:20 -0400

    [CLASE 9]

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 import static ui.UIMenu.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodriguez";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
-
         showMenu();
+
     }
 }
